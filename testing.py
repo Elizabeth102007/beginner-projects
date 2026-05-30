a5cda83f21db7f7776ee43d6dd5d6289311311f6
Elizabeth102007 <drjenniferelizabeth@gmail.com>
2026-05-30 23:00:02 +0300

    Added new mini-projects and exercises

--- testing.py
+++
@@ -1,7 +0,0 @@
-def invert(dictionary: dict):
-    inverted = {value: key for key, value in dictionary.items()}
-    dictionary.clear()
-    dictionary.update(inverted)
-    print(dictionary)
-if __name__=="__main__":
-    invert({1: "first", 2: "second", 3: "third", 4: "fourth"})
