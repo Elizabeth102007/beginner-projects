c10ce53afe5ff7ea0407f711e8abe852e376e7a3
Elizabeth102007 <drjenniferelizabeth@gmail.com>
2026-06-15 20:08:02 +0300

    Added few projects and exercises

---
+++ contact_book_cli_v2.py
@@ -0,0 +1,110 @@
+import json
+file_path = "contact_book.json"
+
+def load_contacts():
+    try:
+        with open(file_path, "r") as file:
+            return json.load(file)
+    except FileNotFoundError:
+        return []
+
+def save_contacts(contacts):
+    with open(file_path, "w") as file:
+        json.dump(contacts, file, indent=2)
+
+def add_contacts(contacts):
+    name = input("Enter your name: ").lower()
+    email= input("Enter your email address: ")
+    phone = input("Enter your phone number: ")
+           
+    person = {
+              "name" : name,
+              "email" : email,
+              "phone" : phone
+              }
+    
+    contacts.append(person)
+    print("Contact updated!")
+
+
+def view_contacts(contacts):
+    if not contacts:
+        print("No contact added")
+    else:
+        for contact in contacts:
+            print(f"Name: {contact['name'].capitalize()}")
+            print(f"Email Address : {contact['email']}")
+            print(f"Phone Number: {contact['phone']}")
+            print("****************************")
+
+
+def search_contacts(contacts):
+    name_search = input("Enter the name you want to search for: ")
+    search = False
+    for contact in contacts:
+        if contact['name'] == name_search.lower():
+            print("Contact found")
+            print(contact['name'])
+            print(contact['email'])
+            print(contact['phone'])
+            search = True
+            break
+    if not search:
+        print(f"{name_search} not found.")
+
+def delete_contacts(contacts):
+    name_delete = input("Enter the name you want to delete: ")
+    delete = False
+    for contact in contacts:
+        if contact['name'] == name_delete.lower():
+            contacts.remove(contact)
+            print("Contact deleted")
+            delete = True
+            break
+    if not delete:
+        print(f"{name_delete} not found.")
+
+
+contacts = load_contacts()
+def menu():
+    print("=====CONTACT BOOK====")
+    print("1. Add contacts")
+    print("2. View contacts")
+    print("3. Search for contacts")
+    print("4. Delete contacts")
+    print("5. Exit")
+    
+def main():
+    print(f"Total Contacts: {len(contacts)}")
+    while True:
+       menu()
+       choice = input("Choose an option(1, 2, 3, 4, 5, 6)  : ")
+       if choice not in ("1", "2", "3", "4", "5", "6"):
+           print("Please check the available choices,and enter a valid one.")
+
+       if choice == "1":
+           add_contacts(contacts)
+           save_contacts(contacts)
+
+
+       elif choice == "2":
+            view_contacts(contacts)
+
+
+       elif choice == "3":
+            search_contacts(contacts)
+
+
+       elif choice == "4":
+             delete_contacts(contacts)
+             save_contacts(contacts)
+
+
+       elif choice == "5":
+         break
+
+if __name__ == "__main__":
+    main()
+
+
+
---
+++ student_grade_tracker.py
@@ -0,0 +1,118 @@
+import csv
+file_path = "grades.csv"
+def load_file():
+   try: 
+       with open(file_path, "r") as file:
+            reader = csv.DictReader(file)
+            return list(reader)
+   
+   except FileNotFoundError:
+       return []
+
+students = load_file()
+
+def save_file(students):
+    with open(file_path, "w", newline="") as file:
+        fieldnames = ["name", "math", "english", "science"]
+        
+        writer = csv.DictWriter(file, fieldnames = fieldnames)
+        
+
+        writer.writeheader()
+
+        for student in students:
+            writer.writerow(student)
+
+
+def add_details(students):
+    name = input("Enter your name: ").lower()
+    math = input("Enter your math score: ")
+    english = input("Enter your english score: ")
+    science = input("Enter your science score: ")
+
+    course = {"name" : name,
+              "math" : math,
+              "english" : english,
+              "science" : science}
+    
+    students.append(course)
+    print("Student update done!")
+
+def view_details(students):
+    if not students:
+        print("No student details added")
+    else:
+        for student in students:
+            print(f"Name: {student['name'].title()}")
+            print(f"Mathematics: {student['math']}")
+            print(f"English: {student['english']}")
+            print(f"Science: {student['science']}")
+            total = int(student['math']) + int(student['english']) + int(student['science'])
+            print(f"Total Average : {total/3:.2f}")
+
+def search_student(students):
+    name_search = input("Enter the name you want to search for: ")
+    search = False
+    for student in students:
+        if student['name'] == name_search.lower():
+            print("Name found!")
+            print(f"Name: {student['name'].title()}")
+            print(f"Mathematics: {student['math']}")
+            print(f"English: {student['english']}")
+            print(f"Science: {student['science']}")
+            search = True
+            break
+    if not search:
+        print(f"{name_search} not found.")
+
+def delete_student(students):
+    name_delete = input("Enter the name you want to delete: ")
+    delete = False
+    for student in students:
+        if student['name'] == name_delete.lower():
+            students.remove(student)
+            print("Student details deleted")
+            delete = True
+            break
+    if not delete:
+        print(f"{name_delete} not found.")
+
+def show_menu():
+    print("===========================")
+    print("1. Add Student details")
+    print("2. View Student details")
+    print("3. Search Student details")
+    print("4. Delete Student details")
+    print("0. Exit")
+    print("============================")
+
+def main():
+    print(f"Total Students: {len(students)}")
+    while True:
+        show_menu()
+        option = input("Select the action you would like to perform: ")
+        
+        if option == "1":
+            add_details(students)
+            save_file(students)
+        
+        elif option == "2":
+            view_details(students)
+        
+        elif option == "3":
+            search_student(students)
+        
+        elif option == "4": 
+            delete_student(students)
+            save_file(students)
+        
+        elif option == "0":
+            print("Goodbye!")
+            break
+
+if __name__ == "__main__":
+    main()
+
+
+
+
